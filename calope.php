75079cbb454deab111f29982b03e9ba2da9a6c02
seancliffordc <123623434+seancliffordc@users.noreply.github.com>
2023-03-28 16:20:56 +0800

    Add files via upload

---
+++ calope.php
@@ -0,0 +1,17 @@
+<hmtl>
+<head><title>Multiplication</title>
+</head>
+<body>
+    <center>
+    <h2>Multiplication Table of 5:</h2>
+    <?php
+        $number = 5;
+
+    for($i=1; $i<=10; $i++){
+            echo $number ." x ". $i ." = ". $number * $i ."<br><br>";
+        // echo "Yeah";
+        }
+    ?>
+    </center>
+</body>
+</html>
